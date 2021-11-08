a0bb1fd2476250c36b4eb4a8c6589d3e763cec24
rehanangular <rehanhanar.com@gmail.com>
2021-11-08 22:14:29 +0530

    2 files modified

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -25,10 +25,9 @@ import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
         style({ height: '0px', minHeight: '0', visibility: 'hidden' })
       ),
       state('expanded', style({ height: '*', visibility: 'visible' })),
-      transition(
-        'expanded <=> collapsed',
-        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
-      ),
+      transition('expanded <=> collapsed', [
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)'),
+      ]),
     ]),
   ],
 })
